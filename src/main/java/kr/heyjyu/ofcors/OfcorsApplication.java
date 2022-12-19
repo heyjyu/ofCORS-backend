944c75d8a714c87687dc64a2c9988036d63b6979
heyjyu <jiwoosmile@yonsei.ac.kr>
2022-12-19 20:51:38 +0900

    프로젝트 세팅하기
    .gitignore 설정하기.
    SpringBoot로 프로젝트 세팅하기.

---
+++ src/main/java/kr/heyjyu/ofcors/OfcorsApplication.java
@@ -0,0 +1,13 @@
+package kr.heyjyu.ofcors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfcorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfcorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/heyjyu/ofcors/OfcorsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.heyjyu.ofcors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfcorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
